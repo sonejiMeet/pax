88b153a9823c729501648e62b6c4f989d8a2670c
sonejiMeet <sonejimeet0@gmail.com>
2025-10-10 18:21:26 +0200

    build fixed on linux

--- build.sh
+++ build.sh
@@ -5,7 +5,11 @@ if [ "$1" == "debug" ]; then
     DEBUG_FLAG="-D_DEBUG"
 fi
 
+cd src/
+
 COMMAND="g++ $DEBUG_FLAG -O2 -w -o main main.cpp lexer.cpp parser.cpp tools.cpp code_manager.cpp c_converter.cpp"
 echo $COMMAND
 
 $COMMAND
+
+cd ..
