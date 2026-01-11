0b1e7a885ed1a87b84d62c84aaaa3a6c05416704
sonejiMeet <sonejimeet0@gmail.com>
2026-01-11 17:18:39 +0100

    linux build fix for single compilation unit also

--- build.sh
+++ build.sh
@@ -7,7 +7,8 @@ fi
 
 cd src/
 
-COMMAND="g++ -ggdb $DEBUG_FLAG -O2 -w -o main main.cpp lexer.cpp parser.cpp tools.cpp code_manager.cpp c_converter.cpp interp.cpp"
+# COMMAND="g++ -ggdb $DEBUG_FLAG -O2 -w -o main main.cpp lexer.cpp parser.cpp tools.cpp code_manager.cpp c_converter.cpp interp.cpp"
+COMMAND="g++ -ggdb $DEBUG_FLAG -w -o pax.exe main.cpp all.cpp"
 echo $COMMAND
 
 $COMMAND
